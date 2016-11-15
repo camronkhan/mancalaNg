8a7d0bcfc2ea07ef9d0a608bd135b9ca56868d1a
Camron Khan <Camron Khan>
2016-11-14 20:59:14 -0600

    added stone count to each pocket

--- src/app/gameboard/gameboard.component.ts
+++ src/app/gameboard/gameboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { GameStartService } from '../services/game-start.service';
 import { Player } from '../models/player';
-import { Gameboard } from '../models/gameboard';
+// import { Gameboard } from '../models/gameboard';
 
 @Component({
   selector: 'app-gameboard',
@@ -12,10 +12,11 @@ import { Gameboard } from '../models/gameboard';
 export class GameboardComponent implements OnInit, OnDestroy {
   private _gameboardVisible: boolean;
   private _subscription: Subscription;
+  private _playerAPockets: Array<number>;
+  private _playerBPockets: Array<number>;
 
   constructor(
     private gameStartService: GameStartService,
-    private _gameboard: Gamepad,
     @Inject('PlayerA') private _playerA: Player,
     @Inject('PlayerB') private _playerB: Player)
   {
@@ -26,9 +27,67 @@ export class GameboardComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this._gameboardVisible = false;
+    this._playerAPockets = [4, 4, 4, 4, 4, 4];
+    this._playerBPockets = [4, 4, 4, 4, 4, 4];
   }
 
   ngOnDestroy() {
     this._subscription.unsubscribe();
   }
+
+  getStonesInPocket(p: string): number {
+        let player: string = p.slice(0, 1).toLowerCase();
+        let pocket: number = Number(p.slice(-1));
+
+        switch (player) {
+            case 'a':
+                if (pocket < 1 || pocket > 6) { throw new Error('Invalid pocket number format'); }
+                return this._playerAPockets[pocket - 1];
+            case 'b':
+                if (pocket < 1 || pocket > 6) { throw new Error('Invalid pocket number format'); }
+                return this._playerBPockets[pocket - 1];
+            default:
+                throw new Error('Invalid pocket letter format');
+        }
+    }
+
+    incrementStonesInPocket(p: string) {
+        let player: string = p.slice(0, 1).toLowerCase();
+        let pocket: number = Number(p.slice(-1));
+
+        switch (player) {
+            case 'a':
+                if (pocket < 1 || pocket > 6) { throw new Error('Invalid pocket number format'); }
+                this._playerAPockets[pocket - 1]++;
+                break;
+            case 'b':
+                if (pocket < 1 || pocket > 6) { throw new Error('Invalid pocket number format'); }
+                this._playerBPockets[pocket - 1]++;
+                break;
+            default:
+                throw new Error('Invalid pocket letter format');
+        }
+    }
+
+    removeStonesFromPocket(p: string): number {
+        let player: string = p.slice(0, 1).toLowerCase();
+        let pocket: number = Number(p.slice(-1));
+        let numStones = 0;
+
+        switch (player) {
+            case 'a':
+                if (pocket < 1 || pocket > 6) { throw new Error('Invalid pocket number format'); }
+                numStones = this._playerAPockets[pocket - 1];
+                this._playerAPockets[pocket - 1] = 0;
+                break;
+            case 'b':
+                if (pocket < 1 || pocket > 6) { throw new Error('Invalid pocket number format'); }
+                this._playerBPockets[pocket - 1] = 0;
+                numStones = this._playerAPockets[pocket - 1];
+                break;
+            default:
+                throw new Error('Invalid pocket letter format');
+        }
+        return numStones;
+    }
 }
