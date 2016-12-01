137f0554de996d36ec4663e54356e2a0e0c92544
Camron Khan <Camron Khan>
2016-11-30 19:12:08 -0600

    added comments

--- src/app/setup/setup.component.ts
+++ src/app/setup/setup.component.ts
@@ -4,38 +4,54 @@ import { GameStartService } from '../services/game-start.service';
 import { Player } from '../models/player';
 
 @Component({
-  selector: 'app-setup',
-  templateUrl: './setup.component.html',
-  styleUrls: ['./setup.component.css']
+    selector: 'app-setup',
+    templateUrl: './setup.component.html',
+    styleUrls: ['./setup.component.css']
 })
 export class SetupComponent implements OnInit, OnDestroy {
-  private _setupVisible: boolean;
-  private _subscription: Subscription;
+    private _setupVisible: boolean;
+    private _subscription: Subscription;
 
-  constructor(
-    private gameStartService: GameStartService,
-    @Inject('PlayerA') private _playerA: Player,
-    @Inject('PlayerB') private _playerB: Player
-  ) { }
+    constructor(
+        private gameStartService: GameStartService,
+        @Inject('PlayerA') private _playerA: Player,
+        @Inject('PlayerB') private _playerB: Player
+    ) { }
 
-  ngOnInit() {
-    this._setupVisible = true;
-  }
-
-  onSubmit() {
-    this._setupVisible = false;
-    this._playerA.turn = true;
-    this.gameStartService.confirmGameStart();
-  }
-
-  ngOnDestroy() {
-    this._subscription.unsubscribe();
-  }
-
-  haveDifferentNames(): boolean {
-    if (this._playerA.name !== this._playerB.name) {
-      return true;
+    /*
+    * Makes setup component visible on startup
+    * Requirement: 
+    */
+    ngOnInit() {
+        this._setupVisible = true;
     }
-    return false;
-  }
+
+    /*
+    * When user clicks start button hide setup compnoent, set Player A to current player, and emit game start observable
+    * Requirement: 
+    */
+    onSubmit() {
+        this._setupVisible = false;
+        this._playerA.turn = true;
+        this.gameStartService.confirmGameStart();
+    }
+
+    /*
+    * Unsubscribe from subscription when component is destroyed
+    * Requirement: 
+    */
+    ngOnDestroy() {
+        this._subscription.unsubscribe();
+    }
+
+    /*
+    * Returns true if string values in player name fields are different, false otherwise
+    * Requirement: 
+    */
+    haveDifferentNames(): boolean {
+        if (this._playerA.name !== this._playerB.name) {
+            return true;
+        }
+        return false;
+    }
 }
