26d7bf651b209f69ea0f007ac08461a745bf25bc
Camron Khan <Camron Khan>
2016-11-30 19:23:28 -0600

    added comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,46 +7,58 @@ import { GameOverService } from './services/game-over.service';
 import { Player } from './models/player';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ GameStartService, GameOverService ]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [ GameStartService, GameOverService ]
 })
 export class AppComponent {
-  title: string = 'mancalaNg';
-  tagline: string = 'play mancala in your web browser';
-  @Input() private _players: Array<Player>;
+    title: string = 'mancalaNg';
+    tagline: string = 'play mancala in your web browser';
+    @Input() private _players: Array<Player>;
 
-  constructor(
-    private _gameStartService: GameStartService,
-    private _gameRestartService: GameRestartService,
-    private _gameOverService: GameOverService
-  ) {
-    this._gameStartService.gameStartConfirmed$.subscribe(p => {
-      this._players = p;
-      this.announceGameStart();
-    });
+    constructor(
+        private _gameStartService: GameStartService,
+        private _gameRestartService: GameRestartService,
+        private _gameOverService: GameOverService
+    ) {
+      this._gameStartService.gameStartConfirmed$.subscribe(p => {
+          this._players = p;
+          this.announceGameStart();
+      });
 
-    this._gameRestartService.gameRestartConfirmed$.subscribe(p => {
-      this._players = p;
-      this.announceGameRestart();
-    });
+      this._gameRestartService.gameRestartConfirmed$.subscribe(p => {
+          this._players = p;
+          this.announceGameRestart();
+      });
 
-    this._gameOverService.gameOverConfirmed$.subscribe(p => {
-      this._players = p;
-      this.announceGameOver();
-    });
-  }
+      this._gameOverService.gameOverConfirmed$.subscribe(p => {
+          this._players = p;
+          this.announceGameOver();
+      });
+    }
 
-  announceGameStart() {
-    this._gameStartService.announceGameStart();
-  }
+    /*
+    * Emits an observable announcing game start
+    * Requirement: 
+    */
+    announceGameStart() {
+        this._gameStartService.announceGameStart();
+    }
 
-  announceGameRestart() {
-    this._gameRestartService.announceGameRestart();
-  }
+    /*
+    * Emits an observable announcing game restart
+    * Requirement: 
+    */
+    announceGameRestart() {
+        this._gameRestartService.announceGameRestart();
+    }
 
-  announceGameOver() {
-    this._gameOverService.announceGameOver();
-  }
+    /*
+    * Emits an observable announcing game over
+    * Requirement: 
+    */
+    announceGameOver() {
+        this._gameOverService.announceGameOver();
+    }
 }
--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -5,63 +5,91 @@ import { GameOverService } from '../services/game-over.service';
 import { Player } from '../models/player';
 
 @Component({
-  selector: 'app-summary',
-  templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.css']
+    selector: 'app-summary',
+    templateUrl: './summary.component.html',
+    styleUrls: ['./summary.component.css']
 })
 export class SummaryComponent implements OnInit, OnDestroy {
-  private _summaryVisible: boolean;
-  private _subscription: Subscription;
-  private _winner: string;
-  private _winnerScore: number;
-  private _loserScore: number;
-  private _isDraw: boolean;
+    private _summaryVisible: boolean;
+    private _subscription: Subscription;
+    private _winner: string;
+    private _winnerScore: number;
+    private _loserScore: number;
+    private _isDraw: boolean;
 
-  constructor(
-    private _gameRestartService: GameRestartService,
-    private _gameOverService: GameOverService,
-    @Inject('PlayerA') private _playerA: Player,
-    @Inject('PlayerB') private _playerB: Player
-  ) {
-    this._gameOverService.gameOverAnnounced$.subscribe(p => {
-      this._winner = this.getWinner();
-      this._winnerScore = this.getWinnerScore();
-      this._loserScore = this.getLoserScore();
-      this._isDraw = this.checkIfDraw();
-      this._summaryVisible = true;
-    });
-  }
+    constructor(
+        private _gameRestartService: GameRestartService,
+        private _gameOverService: GameOverService,
+        @Inject('PlayerA') private _playerA: Player,
+        @Inject('PlayerB') private _playerB: Player
+    ) {
+        this._gameOverService.gameOverAnnounced$.subscribe(p => {
+            this._winner = this.getWinner();
+            this._winnerScore = this.getWinnerScore();
+            this._loserScore = this.getLoserScore();
+            this._isDraw = this.checkIfDraw();
+            this._summaryVisible = true;
+        });
+    }
 
-  ngOnInit() {
-    this._summaryVisible = false;
-  }
+    /*
+    * Hides summary component when initialized
+    * Requirement: 
+    */
+    ngOnInit() {
+        this._summaryVisible = false;
+    }
 
-  onSubmit() {
-    this._summaryVisible = false;
-    this._gameRestartService.confirmGameRestart();
-  }
+    /*
+    * Hides summary component and emits game restart observable when restart button clicked
+    * Requirement: 
+    */
+    onSubmit() {
+        this._summaryVisible = false;
+        this._gameRestartService.confirmGameRestart();
+    }
 
-  ngOnDestroy() {
-    this._subscription.unsubscribe();
-  }
+    /*
+    * Unsubscribes from subscription when component is destroyed
+    * Requirement: 
+    */
+    ngOnDestroy() {
+        this._subscription.unsubscribe();
+    }
 
-  getWinner(): string {
-    if (this._playerA.score > this._playerB.score) { return this._playerA.name; }
-    return this._playerB.name;
-  }
+    /*
+    * Returns player with higher score
+    * Requirement: 
+    */
+    getWinner(): string {
+        if (this._playerA.score > this._playerB.score) { return this._playerA.name; }
+        return this._playerB.name;
+    }
 
-  getWinnerScore(): number {
-    if (this._playerA.score > this._playerB.score) { return this._playerA.score; }
-    return this._playerB.score;
-  }
+    /*
+    * Returns the winner's score
+    * Requirement: 
+    */
+    getWinnerScore(): number {
+        if (this._playerA.score > this._playerB.score) { return this._playerA.score; }
+        return this._playerB.score;
+    }
 
-  getLoserScore(): number {
-    if (this._playerA.score > this._playerB.score) { return this._playerB.score; }
-    return this._playerA.score;
-  }
+    /*
+    * Returns the loser's score
+    * Requirement: 
+    */
+    getLoserScore(): number {
+        if (this._playerA.score > this._playerB.score) { return this._playerB.score; }
+        return this._playerA.score;
+    }
 
-  checkIfDraw(): boolean {
-    if (this._playerA.score === this._playerB.score) { return true; }
-    return false;
-  }
+    /*
+    * Returns true if players tied, false otherwise
+    * Requirement: 
+    */
+    checkIfDraw(): boolean {
+        if (this._playerA.score === this._playerB.score) { return true; }
+        return false;
+    }
 }
